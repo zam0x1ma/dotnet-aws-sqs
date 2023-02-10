84bf38e4c12bc5e0782f00875a71d959fa31adaf
zam0x1ma <zamoxima@protonmail.com>
2023-02-10 16:50:56 +1300

    Create publisher and consumer

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "jane@doe.com",
+    FullName = "Jane Doe",
+    DateOfBirth = new DateTime(2000, 1, 1),
+    GitHubUsername = "janedoe"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
